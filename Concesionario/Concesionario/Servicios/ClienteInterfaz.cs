1633b88cb5fbcd93db5a51f10e49bce15bef613b
David Melendez <145343465+dmelnez@users.noreply.github.com>
2024-02-01 17:02:43 +0100

    Add files via upload

---
+++ Concesionario/Concesionario/Servicios/ClienteInterfaz.cs
@@ -0,0 +1,33 @@
+﻿using Concesionario.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Concesionario.Servicios
+{
+    internal interface ClienteInterfaz
+    {
+        /// <summary>
+        /// Metodo encargado de solicitar al cliente todos los datos que lo compondran al mismo
+        /// <author>DMN - 01/02/2024</author>
+        /// </summary>
+        /// <param name="listaClientes"></param>
+        public void altaCliente(List<Cliente> listaClientes);
+
+        /// <summary>
+        /// Metodo encargado de idntificar al client en base al dni, y validarlo
+        /// <author>DMN - 01/02/2024</author>
+        /// </summary>
+        /// <param name="listaClientes"></param>
+        public void validarCliente(List<Cliente> clientes);
+
+        /// <summary>
+        /// Metodo encargado de modificar identificar al cliente en base al dni, y modificar el campo especificado
+        /// <author>DMN - 01/02/2024</author>
+        /// </summary>
+        /// <param name="listaClientes"></param>
+        public void modificarCliente(List<Cliente> listaClientes, int seleccioMenu);
+    }
+}
---
+++ Concesionario/Concesionario/Servicios/MenuInterfaz.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Concesionario.Servicios
+{
+    internal interface MenuInterfaz
+    {
+        /// <summary>
+        /// Metodo encargado de mostrar el menu principal
+        /// <author>DMN - 01/02/2024</author>
+        /// </summary>
+        /// <param name="listaClientes"></param>
+        public int MenuPrincipla();
+
+        /// <summary>
+        /// Metodo encargado de mostrar el menu de los campos a modificar del cliente
+        /// <author>DMN - 01/02/2024</author>
+        /// </summary>
+        /// <param name="listaClientes"></param>
+        public int modMenuCliente();
+    }
+}
